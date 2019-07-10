0a7e5797693342e96b36ca41489102ad6e74bf07
Yakov Shkolnikov <yakov.shkolnikov@itomy.ch>
2019-07-10 19:21:04 +0300

    DDOS-112 - refactoring

--- DashdevsNetworking/Classes/Authorization/Authorization.swift
+++ DashdevsNetworking/Classes/Authorization/Authorization.swift
@@ -5,8 +5,6 @@
 //  Copyright (c) 2019 dashdevs.com. All rights reserved.
 //
 
-import Foundation
-
 /// This protocol declare method for updating URLRequestComponents with authorization parameters
 public protocol Authorization {
     func authorize(_ requestComponents: inout URLRequestComponents)
