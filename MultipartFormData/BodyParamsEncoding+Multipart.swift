e912a982eef3914b4e416be482e1781c956b1436
Yakov Shkolnikov <yakov.shkolnikov@itomy.ch>
2020-04-28 10:27:56 +0300

    DDOS-191 - added possibility to send many files via multipart

--- MultipartFormData/BodyParamsEncoding+Multipart.swift
+++ MultipartFormData/BodyParamsEncoding+Multipart.swift
@@ -33,3 +33,32 @@ public extension BodyParamEncoding where A == MultipartFileParameters {
         }, headers: [HTTPHeader.multipartFormData(with: boundaryBuilder.boundary)])
     }
 }
+
+public extension BodyParamEncoding where A == [MultipartFileParameters] {
+    
+    /// Factory method which returns pre-defined object for encoding multipart parameters
+    ///
+    /// - Returns: object for encoding parameters
+    static var multipart: BodyParamEncoding {
+        let boundaryBuilder = BoundaryBuilder()
+        return BodyParamEncoding({ encodables -> Data? in
+            do {
+                var data = Data()
+                try encodables.forEach { encodable in
+                    let contentBuilder = MultipartContentBuilder(name: encodable.name, fileName: encodable.fileName, mimeType: encodable.mimeType)
+                    guard let crlf = EncodingCharacters.crlf.data(using: .utf8) else { return }
+                    data.append(try boundaryBuilder.buildPrefix())
+                    data.append(try contentBuilder.buildContentDisposition())
+                    data.append(try contentBuilder.buildContentType())
+                    data.append(crlf)
+                    data.append(try Data(contentsOf: encodable.fileURL, options: []))
+                    data.append(crlf)
+                }
+                data.append(try boundaryBuilder.buildSuffix())
+                return data
+            } catch {
+                return nil
+            }
+        }, headers: [HTTPHeader.multipartFormData(with: boundaryBuilder.boundary)])
+    }
+}
