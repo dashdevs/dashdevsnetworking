309edd21d99e1c51fb1dc78dd02a900db278c957
Yakov Shkolnikov <yakov.shkolnikov@itomy.ch>
2020-04-29 12:51:27 +0300

    DDOS-201 - added presenting image picker view controller and request descriptor to upload image

--- Example/DashdevsNetworking/Multipart/MultipartViewController.swift
+++ Example/DashdevsNetworking/Multipart/MultipartViewController.swift
@@ -6,7 +6,46 @@
 //
 
 import UIKit
+import DashdevsNetworking
+import Photos
 
-class MultipartViewController: UIViewController {
+class MultipartViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // http://ptsv2.com/t/5qsb7-1588148738 - it's place where you can see that your request was successfully send
     
+    let networkClient = NetworkClient(URL(string: "http://ptsv2.com")!)
+    
+    @IBAction func uploadImage() {
+        processPhotoLibraryPermission { [weak self] in
+            guard let strongSelf = self else { return }
+            let picker = UIImagePickerController()
+            picker.delegate = strongSelf
+            picker.sourceType = .savedPhotosAlbum
+            strongSelf.present(picker, animated: true)
+        }
+    }
+    
+    func uploadImage(with url: URL) {
+        let fileParams = MultipartFileParameters(fileURL: url, name: "file", fileName: "image", mimeType: "image/jpeg")
+        let requestDescriptor = MultipartRequestDescriptor(parameters: fileParams)
+        networkClient.send(requestDescriptor) { result, response in
+            
+        }
+    }
+}
+
+extension MultipartViewController {
+    private func processPhotoLibraryPermission(onAuthorized: @escaping () -> Void) {
+        switch PHPhotoLibrary.authorizationStatus() {
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization { status in
+                DispatchQueue.main.async {
+                    if status == PHAuthorizationStatus.authorized {
+                        onAuthorized()
+                    }
+                }
+            }
+        case .authorized: onAuthorized()
+        default: return
+        }
+    }
 }
