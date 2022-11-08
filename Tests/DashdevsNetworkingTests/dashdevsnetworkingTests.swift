8c34b5be160990378e76c49d4885e6a437fb3017
k4T4NZ4meow <>
2022-11-08 09:52:34 +0200

    DDOS-313 fixed tests name

--- Tests/DashdevsNetworkingTests/dashdevsnetworkingTests.swift
+++ Tests/DashdevsNetworkingTests/dashdevsnetworkingTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import DashdevsNetworking
 
-final class dashdevsnetworkingTests: XCTestCase {
+final class DashdevsNetworkingTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
